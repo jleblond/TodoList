db61d8bd8967bc82abe7359b0a274971be8c59fb
jleblond <jasmine.leblondc@gmail.com>
2019-01-27 13:17:16 -0500

    cleanup

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -3,13 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Input;
 
 use App\Http\Requests\TasksRequest;
 use App\Repositories\TasksRepository;
 
-use App\Task;
 
 class TasksController extends Controller
 {
